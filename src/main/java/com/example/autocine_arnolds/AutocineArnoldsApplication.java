3c2e20616dc904dc3c62f253951cca64c81c29ba
YeisonMC <yeisonmacalupu01@gmail.com>
2023-09-11 11:33:29 -0500

    tarea

---
+++ src/main/java/com/example/autocine_arnolds/AutocineArnoldsApplication.java
@@ -0,0 +1,13 @@
+package com.example.autocine_arnolds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocineArnoldsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocineArnoldsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autocine_arnolds/AutocineArnoldsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autocine_arnolds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocineArnoldsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
